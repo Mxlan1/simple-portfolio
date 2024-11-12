70869c4fcacb8073753a4b5773c8671d71817846
Mxlan1 <131815897+Mxlan1@users.noreply.github.com>
2024-11-12 19:17:22 +0100

    upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("contactButton").addEventListener("click", function() {
+    alert("Bedankt voor je interesse! Stuur een e-mail naar voorbeeld@voorbeeld.com voor meer informatie.");
+});
